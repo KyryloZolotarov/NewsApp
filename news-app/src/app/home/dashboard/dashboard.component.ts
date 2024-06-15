13ae9e52a0e4740bdbc067601669f829f792291e
Kyrylo Zolotarov <zlatashay@gmail.com>
2024-06-15 14:34:08 +0300

    search fixed

--- news-app/src/app/home/dashboard/dashboard.component.ts
+++ news-app/src/app/home/dashboard/dashboard.component.ts
@@ -12,6 +12,7 @@ import { SpaceflightNewsService } from '../../services/spaceflight-news.service'
 import { IArticle } from './Interfaces/article';
 import { CommonModule } from '@angular/common';
 import { IQueryParams } from '../../services/interfaces/queryParams';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -58,64 +59,73 @@ export class DashboardComponent implements OnInit, OnChanges {
     this.query.limit = this.articlesLimit;
     this.spaceflightNewsService.searchArticles(this.query).subscribe(
       (response) => {
-        this.articles = response.results;
+        this.filteredArticles = response.results;
         this.totalArticles = response.count;
+        if (this.filteredArticles.length < 6) {
+          this.isNextPageAvailable = false;
+        } else {
+          this.isNextPageAvailable = true;
+        }
+      },
+      (error) => {
+        console.error('Error searching articles:', error);
+      }
+);
+
+  }
+
+  searchArticleV1() {
+    this.query.offset = 0;
+    this.query.limit = Number.MAX_SAFE_INTEGER;
+    this.query.title_contains_one = this.search.replace(/[^\wа-яА-ЯіІїЇєЄ]+/g, ',');
+  
+    let titleSearch$ = this.spaceflightNewsService.searchArticles(this.query);
+    const query2: IQueryParams = {
+      offset: this.query.offset,
+      limit: this.query.limit,
+      summary_contains_one: this.search.replace(/[^\wа-яА-ЯіІїЇєЄ]+/g, ',')
+    };
+    let summarySearch$ = this.spaceflightNewsService.searchArticles(query2);
+  
+    forkJoin([titleSearch$, summarySearch$]).subscribe(
+      ([titleResponse, summaryResponse]) => {
+        this.articles = titleResponse.results;
+        this.filteredArticles = summaryResponse.results;
+  
+        this.articles = [...this.articles, ...this.filteredArticles];
+  
+        let seenIds: Record<number, boolean> = {};
+        let uniqueArticles = this.articles.filter(article => {
+          if (seenIds.hasOwnProperty(article.id)) {
+            return false; // Возвращаем false, если id уже был встречен
+          } else {
+            seenIds[article.id] = true; // Запоминаем, что встретили этот id
+            return true; // Возвращаем true, чтобы оставить этот объект в результирующем массиве
+          }
+        });
+  
+        this.articles = uniqueArticles;
+  
+        this.articlesForSearchBypage();
       },
       (error) => {
         console.error('Error searching articles:', error);
       }
     );
-    if (this.articles.length < 6) {
-      this.isNextPageAvailable = false;
-    }
-
   }
 
-  async searchArticleV1() {
-    this.query.offset = 0;    
-    this.query.limit = Number.MAX_SAFE_INTEGER;
-      this.query.title_contains_one = this.search.replace(/[^\wа-яА-ЯіІїЇєЄ]+/g, ',');;
-      this.spaceflightNewsService.searchArticles(this.query).subscribe(
-        (response) => {
-          this.articles = [...this.articles, ...response.results];
-        },
-        (error) => {
-          console.error('Error searching articles:', error);
-        }
-      );
-      this.query.title_contains_one = ""
-      this.query.summary_contains_one = this.search.replace(/[^\wа-яА-ЯіІїЇєЄ]+/g, ',');;
-      this.spaceflightNewsService.searchArticles(this.query).subscribe(
-        (response) => {
-          this.articles = [...this.articles, ...response.results];
-        },
-        (error) => {
-          console.error('Error searching articles:', error);
-        }
-      );
-      this.query.summary_contains_one = "";
-      let seenIds: Record<number, boolean> = {};
-      console.log(this.articles);
-      let uniqueArticles = this.articles.filter(article => {
-      if (seenIds.hasOwnProperty(article.id)) {
-        return false; // Возвращаем false, если id уже был встречен
-      } else {
-        seenIds[article.id] = true; // Запоминаем, что встретили этот id
-        return true; // Возвращаем true, чтобы оставить этот объект в результирующем массиве
-      };
-    });
-    this.articles = uniqueArticles;
-      console.log(uniqueArticles);
-      this.articles = this.articles.splice(
+  articlesForSearchBypage(){
+    this.filteredArticles = this.articles.slice(
       this.articlesOffset,
-      this.articlesLimit
+      this.articlesOffset + this.articlesLimit
     );
+    
     this.totalArticles = this.articles.length;
-    if (this.articles.length < this.articlesLimit) {
-      this.isNextPageAvailable = false;
-    } else {
-      this.isNextPageAvailable = true;
-    }
+        if (this.filteredArticles.length < this.articlesLimit) {
+          this.isNextPageAvailable = false;
+        } else {
+          this.isNextPageAvailable = true;
+        }
   }
 
   get totalPages(): number {
@@ -127,12 +137,18 @@ export class DashboardComponent implements OnInit, OnChanges {
     if(this.search === ""){
       this.getArticles();
     }
+    else{
+      this.articlesForSearchBypage();
+    }
   }
 
   onNextPageChange() {
     this.articlesOffset = this.articlesOffset + this.articlesLimit;
     if(this.search === ""){
       this.getArticles();
+    }
+    else{
+      this.articlesForSearchBypage();
     }
   }
 }
