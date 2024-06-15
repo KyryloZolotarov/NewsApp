0927ae2252d72336e586bf27df6e47fc40d56a1b
Kyrylo Zolotarov <zlatashay@gmail.com>
2024-06-15 16:11:12 +0300

    all bugs fixed

--- news-app/src/app/home/dashboard/card/card.component.ts
+++ news-app/src/app/home/dashboard/card/card.component.ts
@@ -3,11 +3,14 @@ import { Router } from '@angular/router';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
 import { IArticle } from '../Interfaces/article';
 import { HighlightPipe } from '../../../highlight.pipe';
+import { CommonModule, DatePipe } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions, HighlightPipe],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent, MatCardActions, CommonModule, HighlightPipe],
+  providers: [DatePipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
